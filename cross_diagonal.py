bef57be5d739631ab8724547e650b8bf2ac33f93
Manojkrishna27 <manojkrishna2725@gmail.com>
2026-02-07 14:43:02 +0530

    cross_diagonal

---
+++ cross_diagonal.py
@@ -0,0 +1,8 @@
+X=[          # reverse diagonal view     
+    [1,2,3], # create a matrix 
+    [4,5,6],
+    [7,8,9]
+]
+n = len(X)      # take range
+for i in range(n):           # 
+    print(" "*i, X[i][n - 1 - i]) # first print space and use this reverse logic
